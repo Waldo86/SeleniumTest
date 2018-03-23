41d7d7d641c672b7ba5932ad26b9d35b97a98a67
Laco <lspak@pixelfederation.com>
2018-03-23 14:38:51 +0100

    Test/MyFirstTest.class Click Me Baby page

--- src/Test/MyFirstTest.java
+++ src/Test/MyFirstTest.java
@@ -1,14 +1,16 @@
 package Test;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class MyFirstTest {
     private WebDriver driver;
-    private final String BASE_URL = "https://www.facebook.com";
+    private final String BASE_URL = "http://localhost/clickmebaby.php";
 
 
     @Before
@@ -20,6 +22,22 @@ public class MyFirstTest {
     @Test
     public void test(){
         driver.get(BASE_URL);
+        Assert.assertEquals("0", driver.findElement(By.id("clicks")).getText());
+        Assert.assertEquals("klikov", driver.findElement(By.className("description")).getText());
+
+        for (int i = 1; i < 11; i++) {
+            driver.findElement(By.id("clickMe")).click();
+            Assert.assertEquals(String.valueOf(i), driver.findElement(By.id("clicks")).getText());
+            if (i==1){
+                Assert.assertEquals("klik", driver.findElement(By.className("description")).getText());
+            }
+            if (i==2 && i<=4){
+                Assert.assertEquals("kliky", driver.findElement(By.className("description")).getText());
+            }
+            if (i>4){
+                Assert.assertEquals("klikov", driver.findElement(By.className("description")).getText());
+            }
+        }
 
     }
 
